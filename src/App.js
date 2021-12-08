996cc67fea74830f2ee493150a85dc00664eb4c5
Sunny Bedi <sunnybedi002@yahoo.com>
2021-12-08 09:29:06 -0800

    mvp met

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
-}
+const StyledFriend = styled.div`
+  width: 60%
+  display: flex;
+  justify-content: space-between;
+  padding: 8px
+  border-bottom: 2px solid white;
+  line-height: 1.6;
+  background-color: ${pr => pr.theme.primaryColor};
+  color: ${pr => pr.theme.white}
+`
+
+
+return (
+  <StyledFriend>
+  <div className="App">
+    <p>
+      Read through the instructions in the README.md file to build your NASA
+      app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+    </p>
+  <h1>{nasaData.title}</h1>
+  <p>{nasaData.date}</p>
+  <div className='body'>
+    <img src={nasaData.url} alt={'Nebula'}/>
+  </div>
+  <p>{nasaData.explanation}</p>
+  </div>
+  </StyledFriend>
+);
+
 
 export default App;
